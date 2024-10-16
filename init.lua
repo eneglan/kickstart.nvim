ff143fdcb2b18d3c5ddedd066384bbaf23328213
Christian Kraft <ck@konzept-4.de>
2024-10-16 16:51:32 +0200

    Änderungen an init

--- init.lua
+++ init.lua
@@ -954,3 +954,6 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
